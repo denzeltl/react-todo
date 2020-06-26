8708df2bb0c2a3de9fdd4d93a9587921cf63345e
Denzel <denzeltlee@gmail.com>
2020-06-26 23:39:23 +0800

    Finish main functionality

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ListItem = ({ task, handleChange }) => {
     return (
-        <li className="list-item">
+        <li className={task.completed ? 'list-item is-completed' : 'list-item'}>
             <input type="checkbox" name="item" id={task.id} checked={task.completed} onChange={() => handleChange(task.id)} />
             <label htmlFor={task.id}>{task.task}</label>
         </li>
