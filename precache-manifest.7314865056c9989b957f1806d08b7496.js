85676c267cc68d59ef42cb55885938c815dd1b5e
Denzel <denzeltlee@gmail.com>
2020-06-28 23:52:08 +0800

    Updates

---
+++ precache-manifest.7314865056c9989b957f1806d08b7496.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8335a05bf1efa0d6e5e894e2d3c9f545",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "8b1eab9a6e52f012b3a5",
+    "url": "/react-todo/static/css/main.fc45c6ed.chunk.css"
+  },
+  {
+    "revision": "286b80ff0cbb83a623ee",
+    "url": "/react-todo/static/js/2.4802328f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.4802328f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b1eab9a6e52f012b3a5",
+    "url": "/react-todo/static/js/main.fc4c2140.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  },
+  {
+    "revision": "ff237ac1e80e76477fa7b46d8636c252",
+    "url": "/react-todo/static/media/check-solid.ff237ac1.svg"
+  },
+  {
+    "revision": "c4eb0788d8f6ca9654dcc030d44382ed",
+    "url": "/react-todo/static/media/trash-solid-red.c4eb0788.svg"
+  },
+  {
+    "revision": "f9cdec3f5cd9c716df459450bd855698",
+    "url": "/react-todo/static/media/trash-solid.f9cdec3f.svg"
+  }
+]);
